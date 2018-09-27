b258a75432b91c1dbcb67f1af2a1c02982d88b1d
Andrés Sánchez <Andrs Snchez@AndrewPC>
2018-09-27 18:20:29 -0500

    He agregado bootstrap y los diferentes tipos de usuarios.

---
+++ src/java/mx/edu/ipn/cecyt9/soldout/model/Rol.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ipn.cecyt9.soldout.model;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Rol {
+    private int idrol;
+    private String rol;
+
+    public int getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(int idrol) {
+        this.idrol = idrol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+}
---
+++ src/java/mx/edu/ipn/cecyt9/soldout/model/Rol_1.java
@@ -0,0 +1,86 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ipn.cecyt9.soldout.model;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Rol_1 implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private Integer idrol;
+    private String rol;
+    private Collection<Usuario> usuarioCollection;
+
+    public Rol_1() {
+    }
+
+    public Rol_1(Integer idrol) {
+        this.idrol = idrol;
+    }
+
+    public Integer getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(Integer idrol) {
+        this.idrol = idrol;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Collection<Usuario> getUsuarioCollection() {
+        return usuarioCollection;
+    }
+
+    public void setUsuarioCollection(Collection<Usuario> usuarioCollection) {
+        this.usuarioCollection = usuarioCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idrol != null ? idrol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Rol_1)) {
+            return false;
+        }
+        Rol_1 other = (Rol_1) object;
+        if ((this.idrol == null && other.idrol != null) || (this.idrol != null && !this.idrol.equals(other.idrol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.edu.ipn.cecyt9.soldout.model.Rol_1[ idrol=" + idrol + " ]";
+    }
+    
+}
